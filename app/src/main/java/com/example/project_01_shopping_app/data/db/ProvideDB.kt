306e303659fc6a5b7011cf294060c9bbbb5ef4c9
jieunnnni <120105216+jieunnnni@users.noreply.github.com>
2023-01-26 21:38:07 +0900

    Update ProvideDB.kt

--- app/src/main/java/com/example/project_01_shopping_app/data/db/ProvideDB.kt
+++ app/src/main/java/com/example/project_01_shopping_app/data/db/ProvideDB.kt
@@ -3,7 +3,7 @@ package com.example.project_01_shopping_app.data.db
 import android.content.Context
 import androidx.room.Room
 
-internal fun provideDB(context: Context): ProductDatabase =
+fun provideDB(context: Context): ProductDatabase =
     Room.databaseBuilder(context, ProductDatabase::class.java, ProductDatabase.DB_NAME).build()
 
-internal fun provideToDoDao(database: ProductDatabase) = database.productDao()
+fun provideProductDao(database: ProductDatabase) = database.productDao()
